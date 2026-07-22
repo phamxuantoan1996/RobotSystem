1d02731abbfe83bbb3b7822f16b547afcf2c01c1
phamxuantoan1996 <phamxuantoandc@gmail.com>
2026-07-22 18:52:53 +0700

    feat(navigator): Define INavigatorDriver interface

---
+++ navigator/ports/INavigatorDriver.hpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <system_error>
+#include <functional>
+#include "navigator/domain/entities/NavigatorState.hpp"
+#include "navigator/domain/events/NavigatorEvent.hpp"
+#include "navigator/domain/value_objects/location.hpp"
+#include "navigator/domain/value_objects/station.hpp"
+
+namespace navigator::ports {
+    class INavigatorDriver {
+        public:
+            virtual ~INavigatorDriver() = default;
+
+            virtual std::error_code connect() = 0;
+            virtual void disconnect() = 0;
+            virtual bool isConnected() const = 0;
+
+            virtual std::error_code goToStation(navigator::domain::value_objects::Station station) = 0;
+            virtual std::error_code goToPoint(navigator::domain::value_objects::Location location) = 0;
+
+            virtual std::error_code cancelNavigation() = 0;
+            virtual std::error_code pauseNavigation() = 0;
+            virtual std::error_code resumeNavigation() = 0;
+
+            virtual std::error_code relocation(navigator::domain::value_objects::Location location,navigator::domain::entities::NavigatorCoordinate coordinate,navigator::domain::entities::NavigatorBackMode back_mode) = 0;
+            virtual std::error_code confirmRelocation() = 0;
+
+            navigator::domain::entities::NavigatorState getState();
+
+            using NavigatorEventCallback = std::function<void(const navigator::domain::events::NavigatorEvent& event)>;
+            virtual void setNavigatorEventCallback(NavigatorEventCallback cb);
+            
+    };
+}
