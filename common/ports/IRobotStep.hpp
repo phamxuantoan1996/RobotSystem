6d75a3235517a836a38279536ecbaac1609e8a44
phamxuantoan1996 <phamxuantoandc@gmail.com>
2026-08-05 18:13:07 +0700

    feat(common): Thêm

--- common/ports/IRobotStep.hpp
+++ common/ports/IRobotStep.hpp
@@ -1,4 +1,19 @@
+#pragma once
+#include <variant>
 namespace common::ports {
+    struct GotoStationStepResult {
+        enum class Result {
+            Success,
+            Failed,
+            Canceled
+        };
+        Result result;
+    };
+    struct UnknowStepResult {};
+    using RobotStepResult = std::variant<
+        UnknowStepResult,
+        GotoStationStepResult
+    >;
     class IRobotStep {
         public:
             virtual ~IRobotStep() = default;
