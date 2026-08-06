8a80602921fe03a9cdb1a4d5294c90dc11a48413
phamxuantoan1996 <phamxuantoandc@gmail.com>
2026-08-06 12:03:00 +0700

    refactor(robot): Thêm chỉ thị modify khi kế thừa

--- navigator/application/use_cases/GoToStationStep.hpp
+++ navigator/application/use_cases/GoToStationStep.hpp
@@ -4,7 +4,7 @@
 #include <memory>
 #include <string>
 namespace navigator::application::use_cases {
-    class GoToStationStep : common::ports::IRobotStep {
+    class GoToStationStep : public common::ports::IRobotStep {
         public:
             GoToStationStep(std::shared_ptr<navigator::application::adapter::NavigatorController> controller,std::string station);
             common::ports::RobotStepResult excute(common::ports::RobotStepResult prevResult) override;
