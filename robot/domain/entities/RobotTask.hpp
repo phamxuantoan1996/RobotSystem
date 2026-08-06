3895c131422a68bc10b98dd02f6bb930cb40e05b
phamxuantoan1996 <phamxuantoandc@gmail.com>
2026-08-06 09:34:08 +0700

    feat(robot): Thêm enum RobotTaskStatus để định nghĩa các trạng thái của mission robot

--- robot/domain/entities/RobotTask.hpp
+++ robot/domain/entities/RobotTask.hpp
@@ -3,6 +3,13 @@
 #include <string>
 #include <vector>
 namespace robot::domain::entities {
+    enum class RobotTaskStatus {
+        Unknown,
+        Running,
+        Completed,
+        Canceled,
+        Error
+    };
     struct RobotTask {
         std::string mission_id;
         std::vector<common::ports::IRobotStep> steps;
