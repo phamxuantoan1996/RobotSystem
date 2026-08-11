caceb207a0f078e2dc971596132d2cd1b7f9eb13
phamxuantoan1996 <phamxuantoandc@gmail.com>
2026-08-11 08:38:31 +0700

    feat(robot): Định nghĩa các chế độ hoạt động và các trạng thái của robot

---
+++ robot/domain/entities/RobotStatus.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace robot::domain::entities {
+    enum class RobotOperationMode {
+        Unknown = -1,
+        Manual,
+        Auto
+    };
+
+    enum class RobotStatusCode {
+        Exception = -1,
+        None = 0,
+        Idle = 1,
+        Active = 2,
+        Stop = 3,
+        PauseManual = 4,
+        PauseCollison = 6,
+        Error = 19
+    };
+}
