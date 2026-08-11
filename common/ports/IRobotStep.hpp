2c2efa1654267f5dbd44831ab153dca1f5d12e7b
phamxuantoan1996 <phamxuantoandc@gmail.com>
2026-08-11 08:18:54 +0700

    feat(common): Thêm thuộc tính actionIndex_ để check mission đang thực hiện đến bước nào

--- common/ports/IRobotStep.hpp
+++ common/ports/IRobotStep.hpp
@@ -16,7 +16,11 @@ namespace common::ports {
     >;
     class IRobotStep {
         public:
+            explicit IRobotStep(int action_index) : actionIndex_(action_index) {}
             virtual ~IRobotStep() = default;
             virtual RobotStepResult excute(RobotStepResult prevResult) = 0;
+            virtual int getActionIndex() = 0;
+        protected:
+            int actionIndex_ = -1;
     };
 }
