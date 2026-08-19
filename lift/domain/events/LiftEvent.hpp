14e6f49d47eacc171570d1a38251d95c268e4585
phamxuantoan1996 <phamxuantoandc@gmail.com>
2026-08-19 14:55:12 +0700

    feat(lift): Thêm event set/clear emergency cho lift.

--- lift/domain/events/LiftEvent.hpp
+++ lift/domain/events/LiftEvent.hpp
@@ -1,26 +1,43 @@
 #pragma once
 
+#include <cstdint>
+#include <string>
 #include <variant>
 namespace lift::domain::events {
     struct LiftStatusInitEvent {};
     struct LiftStatusIdleEvent {};
     struct LiftStatusBusyEvent {};
-    struct LiftStarusErrorEvent {};
+    struct LiftStatusSetErrorEvent {
+        uint8_t error_code;
+        std::string reason;
+    };
+    struct LiftStatusClearErrorEvent {
+        uint8_t error_code;
+    };
+
+    struct LiftStatusSetEmergency {};
+    struct LiftStatusClearEmergency {};
 
     struct LiftTaskRunningEvent {};
     struct LiftTaskCompleteEvent {};
     struct LiftTaskCancelEvent {};
-    struct LiftTaskErrorEvent {};
+    struct LiftTaskPausedEvent {
+        uint8_t error_code;
+        std::string reason;
+    };
 
     using LiftEvent = std::variant<
         LiftStatusInitEvent,
         LiftStatusIdleEvent,
         LiftStatusBusyEvent,
-        LiftStarusErrorEvent,
+        LiftStatusSetErrorEvent,
+        LiftStatusClearErrorEvent,
+        LiftStatusSetEmergency,
+        LiftStatusClearEmergency,
 
         LiftTaskRunningEvent,
         LiftTaskCompleteEvent,
         LiftTaskCancelEvent,
-        LiftTaskErrorEvent
+        LiftTaskPausedEvent
     >;
 }
